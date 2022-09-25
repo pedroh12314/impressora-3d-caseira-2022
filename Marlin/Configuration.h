e9e1cb90deb6c3625c32bd1ded356b05a270748f
pedroh12314 <pedroh.12314@gmail.com>
2022-09-25 17:28:43 -0300

    Fix passos Eixos Y e Z

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -608,7 +608,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.56, 63, 2275.55, 92.6 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.56, 64.31, 2275.55, 92.6 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -623,7 +623,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 400, 400, 100, 10000 } // default  { 800, 800, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 600, 600, 100, 10000 } // default  { 800, 800, 100, 10000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
