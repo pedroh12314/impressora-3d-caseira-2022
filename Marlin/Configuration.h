ab77c5a017babc1c5bb80f14033e2440ae3f3b89
pedroh12314 <pedroh.12314@gmail.com>
2022-09-25 15:07:18 -0300

    Fix distacia eixos Y e Z

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -608,7 +608,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.56, 60, 2560, 92.6 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.56, 63, 2275.55, 92.6 }
 
 /**
  * Default Max Feed Rate (mm/s)
